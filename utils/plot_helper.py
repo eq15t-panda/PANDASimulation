f4439a02415d120852246570c76214e6d4a579eb
Chris Ze Third <william.genetelli@gmail.com>
2026-06-29 19:04:04 +0200

    Improved colormap in stability plot for better readability

--- utils/plot_helper.py
+++ utils/plot_helper.py
@@ -1,20 +1,35 @@
 from itertools import chain
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
 
 
-def flip(items, ncol):
+def cmap_map(function, cmap):
+    """ Applies function (which should operate on vectors of shape 3: [r, g, b]), on colormap cmap.
+    This routine will break any discontinuous points in a colormap.
     """
-    Flips the order of items in a list by grouping them into columns.
-    *used to fill columns of legends first*
+    cdict = cmap._segmentdata
+    step_dict = {}
+    # Firt get the list of points where the segments start or end
+    for key in ('red', 'green', 'blue'):
+        step_dict[key] = list(map(lambda x: x[0], cdict[key]))
+    step_list = sum(step_dict.values(), [])
+    step_list = np.array(list(set(step_list)))
+    # Then compute the LUT, and apply the function to the LUT
+    reduced_cmap = lambda step : np.array(cmap(step)[0:3])
+    old_LUT = np.array(list(map(reduced_cmap, step_list)))
+    new_LUT = np.array(list(map(function, old_LUT)))
+    # Now try to make a minimal segment definition of the new LUT
+    cdict = {}
+    for i, key in enumerate(['red','green','blue']):
+        this_cdict = {}
+        for j, step in enumerate(step_list):
+            if step in step_dict[key]:
+                this_cdict[step] = new_LUT[j, i]
+            elif new_LUT[j,i] != old_LUT[j, i]:
+                this_cdict[step] = new_LUT[j, i]
+        colorvector = list(map(lambda x: x + (x[1], ), this_cdict.items()))
+        colorvector.sort()
+        cdict[key] = colorvector
 
-    Parameters:
-        items (list): The list of items to be flipped.
-        ncol (int): The number of columns to group the items into.
-
-    Returns:
-        itertools.chain: A chain object containing the flipped items.
-
-    Source:
-        https://stackoverflow.com/questions/10101141/
-        matplotlib-legend-add-items-across-columns-instead-of-down
-    """
-    return chain(*[items[i::ncol] for i in range(ncol)])
+    return matplotlib.colors.LinearSegmentedColormap('colormap',cdict,1024)
